f8d24c1da694240b1818ab9834336641e229b28d
Gearlify <gearlifycorporation@gmail.com>
2025-02-21 13:57:16 -0800

    register.php

--- register.php
+++ register.php
@@ -9,48 +9,39 @@ if(isset($_POST['signUp'])){
     $password=$_POST['password'];
     $password=md5($password);
 
-     $checkEmail = $conn->prepare("SELECT * FROM users WHERE email = ?");
-     $checkEmail->bind_param("s", $email);
-     $checkEmail->execute();
-     $result = $checkEmail->get_result();
-     if($result->num_rows>0){
+    $checkEmail = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $checkEmail->bind_param("s", $email);
+    $checkEmail->execute();
+    $result = $checkEmail->get_result();
+    if($result->num_rows>0){
         echo "Email Address Already Exists !";
+    } else {
         $insertQuery = $conn->prepare("INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)");
         $insertQuery->bind_param("ssss", $firstName, $lastName, $email, $password);
         if ($insertQuery->execute() === TRUE) {
-                       VALUES ('$firstName','$lastName','$email','$password');
-            if($conn->query($insertQuery)==TRUE){
-                header("location: index.php");
-            }
-            else{
-                echo "Error:".$conn->error;
-            }
-     }
-   
-
+            header("location: index.php");
+        } else {
+            echo "Error:".$conn->error;
+        }
+    }
 }
 
 if(isset($_POST['signIn'])){
-   $email=$_POST['email'];
-   $password=$_POST['password'];
-   $sql = $conn->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
-   $sql->bind_param("ss", $email, $password);
-   $sql->execute();
-   $result = $sql->get_result();
-   $password = md5($password);
-   $sql->bind_param("ss", $email, $password);
-   $sql->execute();
-   $result = $sql->get_result();
-   if($result->num_rows>0){
-    session_start();
-    $row=$result->fetch_assoc();
-    $_SESSION['email']=$row['email'];
-    header("Location: homepage.php");
-    exit();
-   }
-   else{
-    echo "Not Found, Incorrect Email or Password";
-   }
-
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $password = md5($password);
+    $sql = $conn->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+    $sql->bind_param("ss", $email, $password);
+    $sql->execute();
+    $result = $sql->get_result();
+    if($result->num_rows>0){
+        session_start();
+        $row=$result->fetch_assoc();
+        $_SESSION['email']=$row['email'];
+        header("Location: homepage.php");
+        exit();
+    } else {
+        echo "Not Found, Incorrect Email or Password";
+    }
 }
 ?>
