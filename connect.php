055ae9a0410404d6d53aa16e80a6256588c80e24
Gearlify <gearlifycorporation@gmail.com>
2025-02-21 13:36:10 -0800

    connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,8 @@
-<?php
 
-$host="localhost";
-$user="root";
-$pass="";
-$db="login";
+$host="sql309.infinityfree.com";
+$user="if0_38369159";
+$pass="YgcpAy5B26dH6ba";
+$db="if0_38369159_loginpage";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
